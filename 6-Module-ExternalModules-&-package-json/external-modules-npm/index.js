b41f2c6aa11b7381aff0849064485d151f29d821
TBS96 <prantik.g1996@gmail.com>
2024-06-29 00:03:18 +0530

    external modules: npm init+install & pack age.json

---
+++ 6-Module-ExternalModules-&-package-json/external-modules-npm/index.js
@@ -0,0 +1,25 @@
+// random-fruits-name pkg:
+
+const randomFruitsName = require('random-fruits-name');
+console.log(randomFruitsName());
+
+
+
+// movies-names pkg:
+
+var moviesNames = require('movies-names');
+console.log(moviesNames.all);
+console.log(moviesNames.random());
+
+
+
+// get-currency-by-country pkg:
+
+const currCountry = require('get-currency-by-country');
+console.log(`Indian currency: ${currCountry.getCurrency('India')}`);
+
+
+
+
+// For npm packages:
+// https://www.npmjs.com/
